1b83c1aae39fdeca7965aa5013da7842d10533d7
그린햄스터 <sohyun941203@gmail.com>
2023-11-17 16:17:39 +0700

    JS - Menu

--- src/main.js
+++ src/main.js
@@ -30,3 +30,15 @@ document.addEventListener('scroll', () => {
         arrowUp.style.opacity = 0;
     }
 });
+
+// toggle nav bar 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
+
+// navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
+})
